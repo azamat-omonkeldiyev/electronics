ce7dd1f4e175f6dc6f4981ce1f9493014ed2f58a
Azamat <azamatomonkeldiyev@gmail.com>
2025-05-02 07:29:23 +0500

    added

--- src/main.ts
+++ src/main.ts
@@ -17,5 +17,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Swagger UI: http://localhost:${process.env.PORT ?? 3000}/api`);
 }
 bootstrap();
